d9def000eca2276e87c796bb1d1405b06b308cf1
Stefan Urech <stefan@surech.ch>
2017-06-05 15:24:38 +0200

    stu: Auch die Root-Ressource /map gibt jetzt eine korrekte HAL-Antwort zurück

---
+++ src/main/java/ch/mapirium/server/mapservice/rest/model/MapListMapper.java
@@ -0,0 +1,34 @@
+package ch.mapirium.server.mapservice.rest.model;
+
+import ch.mapirium.server.mapservice.model.MapEntity;
+import ch.mapirium.server.mapservice.rest.controller.MapRestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
+
+/**
+ * Mappt eine Liste von Karten auf eine REST-Ressource
+ */
+@Component
+public class MapListMapper {
+
+    @Autowired
+    private MapMapper mapMapper;
+
+    public MapListResource fromEntity(Iterable<MapEntity> entities) {
+        MapListResource result = new MapListResource();
+        result.add(linkTo(methodOn(MapRestController.class).getAll()).withSelfRel());
+
+        // Die einzelnen Entitäten mappen
+        List<MapResource> mapResources = StreamSupport.stream(entities.spliterator(), true).map(mapMapper::fromEntity).collect(Collectors.toList());
+        result.embed("maps", mapResources);
+
+        return result;
+    }
+}
---
+++ src/main/java/ch/mapirium/server/mapservice/rest/model/MapListResource.java
@@ -0,0 +1,10 @@
+package ch.mapirium.server.mapservice.rest.model;
+
+import ch.mapirium.server.common.springmvc.rest.HalResource;
+import org.springframework.hateoas.ResourceSupport;
+
+/**
+ * Hülle für eine Liste von Karten
+ */
+public class MapListResource extends HalResource {
+}
